4e98b9d2cada9e4fe79c4b651c976f677431ad39
mrange <marten_range@hotmail.com>
2013-06-18 21:08:19 +0200

    1. Added AuthorizationRequired exception

---
+++ Presentations/MetaProgrammingWithT4/Demos/Demo_001/AuthorizationRequiredException.cs
@@ -0,0 +1,54 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) WCOM AB.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace Demo_001
+{
+    [Serializable]
+    public class AuthorizationRequiredException : Exception
+    {
+        public string ResourceReferenceProperty { get; set; }
+ 
+        public AuthorizationRequiredException()
+        {
+        }
+ 
+        public AuthorizationRequiredException(string message)
+            : base(message)
+        {
+        }
+ 
+        public AuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+ 
+        protected AuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            ResourceReferenceProperty = info.GetString("ResourceReferenceProperty");
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException("info");
+            info.AddValue("ResourceReferenceProperty", ResourceReferenceProperty);
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+}
